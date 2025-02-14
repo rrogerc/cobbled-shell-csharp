146868385f92c9f5198416f263b88b1282672e5f
Roger Chen <rogerchen803@gmail.com>
2025-02-14 12:02:10 -0700

    repl

--- src/main.cs
+++ src/main.cs
@@ -1,10 +1,12 @@
 using System.Net;
 using System.Net.Sockets;
 
-// Uncomment this line to pass the first stage
-Console.Write("$ ");
 
-var s = Console.ReadLine();
+while (true)
+{
+    Console.Write("$ ");
 
-Console.Write($"{s}: command not found");
+    var s = Console.ReadLine();
 
+    Console.Write($"{s}: command not found");
+}
