47a6e4d7c67544bb4b2413f1e0be46be03601640
Roger Chen <rogerchen803@gmail.com>
2025-02-15 18:27:28 -0700

    exit builtin

--- src/main.cs
+++ src/main.cs
@@ -1,12 +1,21 @@
 using System.Net;
 using System.Net.Sockets;
 
-
 while (true)
 {
     Console.Write("$ ");
 
     var s = Console.ReadLine();
+    if (s == null)
+        continue;
+
+    var cmd = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+    if (cmd.Length == 0)
+        continue;
+
+    if (cmd[0] == "exit")
+        Environment.Exit(int.Parse(cmd[1]));
 
     Console.Write($"{s}: command not found\n");
 }
